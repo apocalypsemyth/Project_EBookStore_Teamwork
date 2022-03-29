e0c69414db2cd0a72fc44432fb19e2a3b8443bff
hongmamasan <hongmamasan@gmail.com>
2022-03-29 16:03:38 +0800

    加入BookContent欄位，在FileDownload建立資料夾

--- EBookStore/BackAdmin/BookDetail.aspx.designer.cs
+++ EBookStore/BackAdmin/BookDetail.aspx.designer.cs
@@ -177,6 +177,24 @@ namespace EBookStore.BackAdmin
         protected global::System.Web.UI.WebControls.Button btnEditImgCancel;
 
         /// <summary>
+        /// ltlBookContent 控制項。
+        /// </summary>
+        /// <remarks>
+        /// 自動產生的欄位。
+        /// 若要修改，請將欄位宣告從設計工具檔案移到程式碼後置檔案。
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Literal ltlBookContent;
+
+        /// <summary>
+        /// txtBookContent 控制項。
+        /// </summary>
+        /// <remarks>
+        /// 自動產生的欄位。
+        /// 若要修改，請將欄位宣告從設計工具檔案移到程式碼後置檔案。
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtBookContent;
+
+        /// <summary>
         /// ltlPrice 控制項。
         /// </summary>
         /// <remarks>
