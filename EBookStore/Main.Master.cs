80b9727da184720c8da1387582c7884f2613f7de
apocalypsemyth <accelerator.apocalypse@gmail.com>
2022-03-29 18:49:12 +0800

    修改Main的樣式

--- EBookStore/Main.Master.cs
+++ EBookStore/Main.Master.cs
@@ -1,5 +1,4 @@
-﻿using EBookStore.Managers;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -10,39 +9,9 @@ namespace EBookStore
 {
     public partial class Main : System.Web.UI.MasterPage
     {
-        private BookManager _bookMgr = new BookManager();
-
         protected void Page_Load(object sender, EventArgs e)
         {
-            //this.ucNavbar.BtnSearchClick += SearchBook;
-        }
 
-        protected void SearchBook(object sender, string searchText)
-        {
-            //var ctphr = this.ContentPlaceHolder1;
-            //var rptList = (Repeater)ctphr.FindControl("rptList");
-            //var plcEmpty = (PlaceHolder)ctphr.FindControl("plcEmpty");
-
-            //if (rptList != null && plcEmpty != null)
-            //{
-            //    var list = this._bookMgr.GetBookList(searchText);
-            //    string keyword = this.Request.QueryString["keyword"];
-            //    var searchList = this._bookMgr.GetSearchResult(keyword);
-
-            //    if (list.Count == 0)
-            //    {
-            //        rptList.Visible = false;
-            //        plcEmpty.Visible = true;
-            //    }
-            //    else
-            //    {
-            //        rptList.Visible = true;
-            //        plcEmpty.Visible = false;
-
-            //        rptList.DataSource = list;
-            //        rptList.DataBind();
-            //    }
-            //}
         }
     }
 }
